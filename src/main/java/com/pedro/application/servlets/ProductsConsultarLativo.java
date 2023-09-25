d5248cfa40628eaa764fee2b893f1951b1855db7
Pedro <pedrohvidals@gmail.com>
2023-09-25 16:55:16 -0300

    [fix] - consertando logica para consultar produtos pelo l_ativo

--- src/main/java/com/pedro/application/servlets/ProductsConsultarLativo.java
+++ src/main/java/com/pedro/application/servlets/ProductsConsultarLativo.java
@@ -1,9 +1,7 @@
 package com.pedro.application.servlets;
 
 import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import com.pedro.domain.ProductService;
-import com.pedro.infrastructure.entities.Product;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,16 +9,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 
-public class ProductsConsultarLativoFalse extends HttpServlet {
+public class ProductsConsultarLativo extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProductService productService = new ProductService();
 
         PrintWriter writer = resp.getWriter();
 
-        boolean lativo = false;
+        String lativo = req.getParameter("lativo");
 
         JsonArray res = productService.filtrarProdutosPorLativo(lativo);
 
